27ad3494aec76a5b813d3db36f2a63c789080c71
jhetpatel <hetalpatel0976@gmail.com>
2020-02-19 23:07:09 -0800

    implemented multiple sub in User.

--- concierge-app/src/Component/User.js
+++ concierge-app/src/Component/User.js
@@ -1,34 +1,32 @@
-import React, { Component } from 'react';
-import Texty from 'rc-texty';
+import React, { Component } from 'react'
+import Texty from 'rc-texty'
 import { subscribe } from 'mqtt-react'
 
 class User extends Component {
-    render() {
+  render() {
+    const { user, data } = this.props
+    console.log(`...${data[0]}`)
 
-        const {user, data} = this.props
-        console.log(`...${data[0]}`)
+    return (
+      <div className="shape tile_1st">
+        <div className="data_place">
+          <Texty
+            type="scaleBig"
+            mode="random"
+            style={{ fontSize: '3.7rem', marginBottom: '25px' }}
+          >
+            {user.name}
+          </Texty>
 
-        return (
-            <div className='shape tile_1st'>
-                <div className="data_place">
-                    <Texty
-                        type='scaleBig'
-                        mode='random'
-                        style={{ fontSize: '3.7rem', marginBottom: '25px' }}>
-                        {user.name}
-                    </Texty>
-
-                    <Texty
-                        type='mask-bottom'
-                        mode='random'>{user.message}
-                    </Texty>
-                </div>
-
-            </div>
-        );
-    }
+          <Texty type="mask-bottom" mode="random">
+            {user.message}
+          </Texty>
+        </div>
+      </div>
+    )
+  }
 }
-User = subscribe({
-    topic: 'space/input'
-  })(User)
-export default User;
+User = subscribe({ topic:   'space/#' })(User)
+User = subscribe({ topic:   'pod/vibe' })(User)
+User = subscribe({topic:    'client/name'})(User)
+export default User
